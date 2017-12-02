3a80501ecf6b28b7fb101a51e8673ec0adbfba5f
Yair <y4000r@gmail.com>
2017-12-03 00:49:43 +0200

    added getNeighbour

--- Squares.cs
+++ Squares.cs
@@ -8,7 +8,7 @@ namespace Quaridor
 {
     class Squares
     {
-        PlayerCell[] SquareMatrix;
+        public PlayerCell[] SquareMatrix;
 
         public Squares()
         {
@@ -27,9 +27,56 @@ namespace Quaridor
             }
         }
 
+        /*
+         * returns the position of the square in direction dir to square.
+         * if the direction is illegal, returns -1;
+        */
         public int getNeighbour(Direction dir, int square)
         {
-            //TODO: complete
+            int res = -1;
+            switch (dir)
+            {
+                case Direction.Down:
+                    if(!(getRowFromSquare(square) >= Board.BOARD_SIZE-1))
+                    {
+                        res = square + Board.BOARD_SIZE;
+                    }
+                    break;
+                case Direction.Left:
+                    if(getColFromSquare(square) != 0)
+                    {
+                        res = square - 1;
+                    }
+                    break;
+                case Direction.Up:
+                    if(!(getRowFromSquare(square) <= 0))
+                    {
+                        res = square - Board.BOARD_SIZE;
+                    }
+                    break;
+                case Direction.Right:
+                    if(getColFromSquare(square) != Board.BOARD_SIZE-1)
+                    {
+                        res = square + 1;
+                    }
+                    break;
+                default:
+                    //TODO: through exception
+                    break;
+            }
+            return res;
+        }
+
+        //return's the i'th index of  square
+        int getRowFromSquare(int square)
+        {
+            return square / Board.BOARD_SIZE;
+        }
+
+        //return's the j'th index of  square
+        int getColFromSquare(int square)
+        {
+            return square % Board.BOARD_SIZE;
         }
 
     }
